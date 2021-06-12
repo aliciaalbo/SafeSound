a15b4c2cebc06a26f968349a32dc36b3b2c2b4f0
Alicia Albo <aliciaalbo@gmail.com>
2021-06-12 07:47:32 -0700

    create CachedLyrics class

--- model.py
+++ model.py
@@ -18,22 +18,30 @@ class Filter(db.Model):
     """stores values for lyrics to exclude"""
     __tablename__ = "filters"
 
-    filter_id = db.Column(db.String, primary_key=True, autoincrement=True)
+    filter_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     filter_name = db.Column(db.String)
     word_list = db.Column(db.String)
 
     db.relationship(User, backref='filters')
 
-class CachedResult():
-    """store results of filters applied to individual songs"""
-    __tablename__ = "cached_results"
+# class CachedResult():
+#     """store results of filters applied to individual songs"""
+#     __tablename__ = "cached_results"
 
-    cached_result_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    track_id = db.Column(db.String)
-    filter_id = db.Column(db.String, db.ForeignKey('filters.filter_id'))
-    pass_status = db.Column(db.Boolean)
+#     cached_result_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+#     track_id = db.Column(db.String)
+#     filter_id = db.Column(db.String, db.ForeignKey('filters.filter_id'))
+#     pass_status = db.Column(db.Boolean)
 
+class CachedLyrics():
+    """stores unique words in lyrics"""
+    __tablename__ = "cached_lyrics"
+
+    track_id = db.Column(db.String, primary_key=True)
+    title = db.Column(db.String)
+    artist = db.Column(db.String)
+    album_art = db.Column(db.String)
 
 
 
