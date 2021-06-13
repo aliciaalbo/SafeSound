f7573a17ae08c9852d44f46f9dcab407fba4ca40
Alicia Albo <aliciaalbo@gmail.com>
2021-06-13 07:23:21 -0700

    define homepage, api, and callback routes; get_email_and_token from spotiyfy func

--- crud.py
+++ crud.py
@@ -6,6 +6,11 @@ from spotipy.oauth2 import SpotifyOAuth
 import os
 import secrets
 
+cid = secrets.cid
+secret = secrets.secret
+client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
 def create_user(email, name, password):
     """add new user to database"""
 
@@ -31,6 +36,31 @@ def create_filter(user_id, filter_name):
 
 def add_filter_term(filter_id, term):
     """adds word or phrase to filter criteria"""
+
+def get_song_info(playlist):
+    """get song titles and id's from Spotify playlist"""
+    response = spotify.playlist_items(playlist,
+                                    fields='items.track.id,items.track.name,items.track.artists,items.track.album,total',
+                                    additional_types=['track'])
+    for track in response['items']:
+        try:
+            tracks.append(track['track']['id'])
+            titles.append(track['track']['name'])
+            album_arts.append(track['track']['album']['images'][0]['url'])
+            artist_name = ''
+            for artist in track['track']['artists']:
+                # print(artist)
+                # print(artist['name'])
+                # try:
+                if artist_name == '':
+                    # print(artist['name'])
+                    artist_name = artist['name']   
+                else:
+                    artist_name = artist_name + ', ' + artist['name']
+            artists.append(artist_name)
+
+        except(TypeError):
+            continue    
     
 def cache_lyrics(lyrics, track_id):
     """creates a set of unique words in lyrics and returns them as a string"""
--- server.py
+++ server.py
@@ -1,25 +1,31 @@
-# from flask import (Flask, render_template, request, flash, session, redirect, jsonify)
+from flask import (Flask, render_template, request, flash, session, redirect, jsonify)
+from jinja2 import StrictUndefined
 from model import connect_to_db
 import crud
-import model
+# import model
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import secrets
-import os
+# import os
 
-# app = Flask(__name__)
-# app.secret_key = secrets.secret_key
-# app.jinja_env.undefined = StrictUndefined
+app = Flask(__name__)
+app.secret_key = secrets.secret_key
+app.jinja_env.undefined = StrictUndefined
 
-# connect_to_db(app)
+connect_to_db(app)
 
 cid = secrets.cid
 secret = secrets.secret
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
+@app.route('/')
+def show_homepage():
+    """show homepage"""
 
+    return render_template('homepage.html')
 
+@app.route('/api')
 def search_for_playlists(term):
     """searches Spotify for playlists, returns playlists"""
     playlists = spotify.search(term, type='playlist')
@@ -30,8 +36,38 @@ def search_for_playlists(term):
 
     return print(playlist_data)
 
-def get_song_titles():
-    """get song titles and id's from Spotify playlist"""
+# return redirect('/')
 
 
+@app.route('/callback')
+def get_email_and_token():
+    if request.args.get('code'):
+        #token = crud.get_spotify_token(request.args.get('code'))
+        token_info = auth_manager.get_access_token(request.args.get('code'), check_cache=False)
+        spotify = spotipy.Spotify(auth_manager=auth_manager)
+        email = spotify.me()["email"]
+        name = spotify.me()["display_name"]
+        spotify_id = spotify.me()["id"]
+        refresh_token = token_info['refresh_token']
+        access_token = token_info['access_token']
+        # set the access token in a session cookie so it will persist and can be grabbed by React through an API call
+        session['access_token'] = access_token
 
+        if crud.get_user_by_email(email):
+            # Do something to login.. send access token to react? 
+            # then if access toekn show save playlist buttn that isn't made yet
+            crud.update_access_token(email, access_token)
+            crud.update_refresh_token(email, refresh_token)
+        else:
+            crud.create_user(email, name, spotify_id, access_token, refresh_token)
+
+        # set the access token cookie for React
+        #response = redirect('/', 302)
+        # secure=True only if using https, otherwise it won't set the cookie
+        #response.set_cookie('access_cookie', access_token, secure=False, httponly=False)
+        #return response
+
+    # email = sp.user(email)
+    # pprint.pprint(email)
+    # # token = crud.get_spotify_credentials(code)
+    return redirect('/')
