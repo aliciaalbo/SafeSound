e038f0e47fb51324cbb8d4ed14e5d51421f80a79
Alicia Albo <aliciaalbo@gmail.com>
2021-07-01 09:04:08 -0700

    connect empty homepage to back end

--- server.py
+++ server.py
@@ -25,49 +25,53 @@ def show_homepage():
 
     return render_template('homepage.html')
 
-@app.route('/api')
-def search_for_playlists(term):
-    """searches Spotify for playlists, returns playlists"""
-    playlists = spotify.search(term, type='playlist')
-    playlist_data = {}
+# @app.route('/api')
+# def search_for_playlists(term):
+#     """searches Spotify for playlists, returns playlists"""
+#     playlists = spotify.search(term, type='playlist')
+#     playlist_data = {}
 
-    for item in playlists:
-        playlist_data[item]['id'] = [item]['name']
+#     for item in playlists:
+#         playlist_data[item]['id'] = [item]['name']
 
-    return print(playlist_data)
+#     return print(playlist_data)
 
 # return redirect('/')
 
 
-@app.route('/callback')
-def get_email_and_token():
-    if request.args.get('code'):
-        #token = crud.get_spotify_token(request.args.get('code'))
-        token_info = auth_manager.get_access_token(request.args.get('code'), check_cache=False)
-        spotify = spotipy.Spotify(auth_manager=auth_manager)
-        email = spotify.me()["email"]
-        name = spotify.me()["display_name"]
-        spotify_id = spotify.me()["id"]
-        refresh_token = token_info['refresh_token']
-        access_token = token_info['access_token']
-        # set the access token in a session cookie so it will persist and can be grabbed by React through an API call
-        session['access_token'] = access_token
+# @app.route('/callback')
+# def get_email_and_token():
+#     if request.args.get('code'):
+#         #token = crud.get_spotify_token(request.args.get('code'))
+#         token_info = auth_manager.get_access_token(request.args.get('code'), check_cache=False)
+#         spotify = spotipy.Spotify(auth_manager=auth_manager)
+#         email = spotify.me()["email"]
+#         name = spotify.me()["display_name"]
+#         spotify_id = spotify.me()["id"]
+#         refresh_token = token_info['refresh_token']
+#         access_token = token_info['access_token']
+#         # set the access token in a session cookie so it will persist and can be grabbed by React through an API call
+#         session['access_token'] = access_token
 
-        if crud.get_user_by_email(email):
-            # Do something to login.. send access token to react? 
-            # then if access toekn show save playlist buttn that isn't made yet
-            crud.update_access_token(email, access_token)
-            crud.update_refresh_token(email, refresh_token)
-        else:
-            crud.create_user(email, name, spotify_id, access_token, refresh_token)
+#         if crud.get_user_by_email(email):
+#             # Do something to login.. send access token to react? 
+#             # then if access toekn show save playlist buttn that isn't made yet
+#             crud.update_access_token(email, access_token)
+#             crud.update_refresh_token(email, refresh_token)
+#         else:
+#             crud.create_user(email, name, spotify_id, access_token, refresh_token)
 
-        # set the access token cookie for React
-        #response = redirect('/', 302)
-        # secure=True only if using https, otherwise it won't set the cookie
-        #response.set_cookie('access_cookie', access_token, secure=False, httponly=False)
-        #return response
+#         # set the access token cookie for React
+#         #response = redirect('/', 302)
+#         # secure=True only if using https, otherwise it won't set the cookie
+#         #response.set_cookie('access_cookie', access_token, secure=False, httponly=False)
+#         #return response
 
-    # email = sp.user(email)
-    # pprint.pprint(email)
-    # # token = crud.get_spotify_credentials(code)
-    return redirect('/')
+#     # email = sp.user(email)
+#     # pprint.pprint(email)
+#     # # token = crud.get_spotify_credentials(code)
+#     return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
