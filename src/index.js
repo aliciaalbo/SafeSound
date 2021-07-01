bdf0f17a45d4e281226c1057797252604710f080
Alicia Albo <aliciaalbo@gmail.com>
2021-07-01 08:39:46 -0700

    moving more files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import React, {useState} from "react";
-import ReactDOM from "react-dom";
-import useStickyState from "./useStickyState";
-import playlistSearch from "./playlistSearch";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-function App() {
-    const [searchTerm, setSeachTerm] = useStickyState("","searchTerm");
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-}
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
