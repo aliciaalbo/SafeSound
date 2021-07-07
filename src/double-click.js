3fc2778cc86df14e54e30ac7c5d93b49f8824fb3
Alicia Albo <aliciaalbo@gmail.com>
2021-07-07 08:00:23 -0700

    implement double click logic

---
+++ src/double-click.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useClickPreventionOnDoubleClick } from "./stop-triggering-onclick-twice-ondoubleclick";
+
+const ClickableBox = ({ onClick, onDoubleClick }) => {
+  const [handleClick, handleDoubleClick] = useClickPreventionOnDoubleClick(onClick, onDoubleClick);
+
+  return (
+    <div onClick={handleClick} onDoubleClick={handleDoubleClick}>
+      Click or double click
+    </div>
+  );
+};
+
+
+const DoubleClickExample = () => (
+  <ClickableBox onClick={() => console.log("on click")} onDoubleClick={() => console.log("on double click")}/>
+);
+
+export default DoubleClickExample;
